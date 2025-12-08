3d2eb940241ced98ac568833ac274a5c4006efb0
Ivan <ivanstefanov.it@gmail.com>
2025-12-08 15:32:29 +0200

    Day 1

---
+++ SwiftUIDay1.playground/Contents.swift
@@ -0,0 +1,69 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+
+var playerName = "Roy"
+playerName = "Dani"
+playerName = "Sam"
+
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
+
+//MARK: Strings
+let actor = "Danzel Washington"
+let filename = "paris.jpg"
+let result = "✅ You Win!"
+let quote = "Then he tapped a sing saying \"Believe\" and walked away."
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+let nameLength = actor.count
+print(nameLength)
+
+print(result.uppercased())
+
+print(movie.hasPrefix("A day"))
+print(filename.hasSuffix(".jpg"))
+
+//MARK: Int
+
+let score = 10
+let bigNumber = 100_000_000
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubledScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+
+var counter = 10
+counter += 5
+counter *= 2
+counter -= 10
+counter /= 2
+
+//MARK: Decimals
+
+let number = 0.1 + 0.2
+print(number)
+
+let double1 = 3.1
+let double2 = 3131.3131
+let double3 = 3.0
+let int1 = 3
+
+var rating = 10.0
+rating += 5
+rating *= 2
+rating -= 10
+rating /= 2
+
